f564e179801c2cf697805dc48b4aeaaf33262df3
Ngettich <chellahngetich@gmail.com>
2024-10-25 21:13:34 +0300

    Add files via upload

---
+++ NewFile1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string names[5]={"Chela","Memoo","Squishy","Snowwhite","Lewi"};
+	for(int i=0;i<5; i++)
+	{
+		cout<<names[i]<<"\n";
+	}
+	return 0;
+}
